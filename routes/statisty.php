60e934186548dadb335c2b9389320310d1ba39c1
Denmouns <landrymoutongo97@gmail.com>
2026-06-30 07:07:21 +0100

    fix(routes)

--- routes/statisty.php
+++ routes/statisty.php
@@ -25,6 +25,8 @@ $webRoutes = static function (): array {
     return (array) config('statisty.routes.web', []);
 };
 
+$modelConstraint = '.*';
+
 $apiRoutesData = $apiRoutes();
 if ((bool) ($apiRoutesData['enabled'] ?? true)) {
     $apiPrefix      = trim((string) ($apiRoutesData['prefix'] ?? 'api/statisty'), '/');
@@ -33,17 +35,27 @@ if ((bool) ($apiRoutesData['enabled'] ?? true)) {
     Route::prefix($apiPrefix)
         ->middleware(array_values(array_merge($apiMiddleware, $sharedMiddleware())))
         ->name('statisty.api.')
-        ->group(function () use ($authMiddleware) {
+        ->group(function () use ($authMiddleware, $modelConstraint) {
             Route::get('health', function () {
                 return response()->json(['status' => 'ok']);
             })->name('health');
 
-            Route::middleware($authMiddleware())->group(function () {
-                Route::get('discovery/{table}', [\Statisty\Http\Controllers\DiscoveryController::class, 'show'])->name('discovery.show');
-                Route::get('charts/{model}',    [\Statisty\Http\Controllers\ChartController::class,     'show'])->name('charts.show');
-                Route::get('tables/{model}',    [\Statisty\Http\Controllers\TableController::class,     'index'])->name('tables.index');
-                Route::get('workspace/{name}',  [\Statisty\Http\Controllers\WorkspaceController::class, 'show'])->name('workspace.show');
-                Route::get('metrics/{model}',   [\Statisty\Http\Controllers\MetricsController::class,   'index'])->name('metrics.index');
+            Route::middleware($authMiddleware())->group(function () use ($modelConstraint) {
+                Route::get('discovery/{table}', [\Statisty\Http\Controllers\DiscoveryController::class, 'show'])
+                    ->where('table', $modelConstraint)
+                    ->name('discovery.show');
+                Route::get('charts/{model}',    [\Statisty\Http\Controllers\ChartController::class,     'show'])
+                    ->where('model', $modelConstraint)
+                    ->name('charts.show');
+                Route::get('tables/{model}',    [\Statisty\Http\Controllers\TableController::class,     'index'])
+                    ->where('model', $modelConstraint)
+                    ->name('tables.index');
+                Route::get('workspace/{name}',  [\Statisty\Http\Controllers\WorkspaceController::class, 'show'])
+                    ->where('name', $modelConstraint)
+                    ->name('workspace.show');
+                Route::get('metrics/{model}',   [\Statisty\Http\Controllers\MetricsController::class,   'index'])
+                    ->where('model', $modelConstraint)
+                    ->name('metrics.index');
             });
         });
 }
@@ -56,10 +68,12 @@ if ((bool) ($webRoutesData['enabled'] ?? true)) {
     Route::prefix($webPrefix)
         ->middleware(array_values(array_merge($webMiddleware, $sharedMiddleware())))
         ->name('statisty.web.')
-        ->group(function () use ($authMiddleware) {
-            Route::middleware($authMiddleware())->group(function () {
+        ->group(function () use ($authMiddleware, $modelConstraint) {
+            Route::middleware($authMiddleware())->group(function () use ($modelConstraint) {
                 Route::get('dashboard', [\Statisty\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-                Route::get('workflow/{model}', [\Statisty\Http\Controllers\WorkflowController::class, 'workflow'])->name('workflow');
+                Route::get('workflow/{model}', [\Statisty\Http\Controllers\WorkflowController::class, 'workflow'])
+                    ->where('model', $modelConstraint)
+                    ->name('workflow');
                 Route::get('health',  [\Statisty\Http\Controllers\HealthController::class, 'health'])->name('health');
                 Route::get('logs',    [\Statisty\Http\Controllers\LogsController::class,   'logs'])->name('logs');
                 Route::get('jobs',    [\Statisty\Http\Controllers\JobsController::class,   'jobs'])->name('jobs');
